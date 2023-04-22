1fcc99dab7a36de997d665869a44099f244f8440
Irina <sekti2004@yandex.ru>
2023-04-22 13:50:14 +0300

    fix bugs

--- 2.c
+++ 2.c
@@ -3,8 +3,7 @@
 #include <string.h>
 
 typedef struct {
-    char n;
-    char *surname;
+    char surname[100];
     char initial[4];
     char gender[1];
     char groupNum[2];
@@ -15,75 +14,69 @@ typedef struct {
 
 int main(void) {
     FILE* fp = fopen("students2.csv", "r");
-    FILE* out = fopen("students.bin", "w");
- 
-    if (!fp)
-        printf("Can't open file\n");
- 
-    else {
-        int n = 100;
-        char buffer[n];
-        int row = 0;
-        int column = 0;
-        while (fgets(buffer,
-                     n, fp)) {
-            column = 0;
-            ++row;
-            if (row == 1)
-                continue;
-            student* s = (student*) malloc (sizeof(student));
-            int index = 0;
-            char surname[100];
-            int count = 0;
-            for (int i = 0; i < n; ++i) {
-                if (buffer[i] == '\n') {
-                    break;
-                }
-                if (buffer[i] == ',') {
-                    ++column;
-                    index = 0;
-                    continue;
-                }
-                if (column == 0) {
-                    ++count;
-                    surname[count - 1] = buffer[i];
-                }
-                if (column == 1) { 
-                    s -> n = count;
-                    s -> surname = surname;
-                    s -> initial[index] = buffer[i];
-                }
-                if (column == 2) {
-                    s -> gender[index] = buffer[i];
-                }
-                if (column == 3) {
-                    s -> groupNum[index] = buffer[i];
-                }
-                if (column == 4) {
-                    s -> sub1[index] = buffer[i];
-                }
-                if (column == 5) {
-                    s -> sub2[index] = buffer[i];
-                }
-                if (column == 6) {
-                    s -> sub3[index] = buffer[i];
-                    break;
-                }
-                ++index;
-            }
-            fwrite(s, sizeof(student), 1, out);
-        }
-        fclose(fp);
-        fclose(out);
-        FILE* in = fopen("students.bin", "r");
-        student* s = (student*) malloc (sizeof(student));
-        int count = 0;
-        while (fread(s, sizeof(student), 1, in) == 1) {
-            if ((s -> sub1[0] != s -> sub2[0]) && (s -> sub1[0] != s -> sub3[0]) && (s -> sub2[0] != s -> sub3[0])) {
-                ++count;
-            }
-        }
-        printf("%d\n", count);
+    FILE* out = fopen("students.bin", "r+b");
+    int length = 0;
+    char s;
+    while (fread(&s, sizeof(char), 1, out) == 1) {
+        ++length;
     }
+    fseek(out, length, SEEK_CUR);
+    int n = 100;
+    char buffer[n];
+    int row = 0;
+    int column = 0;
+    while (fgets(buffer, n, fp)) {
+        column = 0;
+        ++row;
+        if (row == 1)
+            continue;
+        char surname[100];
+        char count = '0';
+        for (int i = 0; i < n; ++i) {
+            if (buffer[i] == '\n') {
+                break;
+            }
+            if (buffer[i] == ',') {
+                ++column;
+                continue;
+            }
+            if (column == 0) {
+                ++count;
+                surname[(count - '0') - 1] = buffer[i];
+            }
+            if (column == 1) { 
+                if (count != -1) {
+                    for (int i = count - '0'; i < 100; ++i) {
+                        surname[i] = ' ';
+                    }
+                    fwrite(&count, sizeof(char), 1, out);
+                    for (int i = 0; i < count - '0'; ++i) {
+                        char symbol = surname[i];
+                        fwrite(&symbol, sizeof(char), 1, out);
+                    }
+                    count = -1;
+                }
+                fwrite(&buffer[i], sizeof(char), 1, out);
+            }
+            if (column == 2) {
+                fwrite(&buffer[i], sizeof(char), 1, out);
+            }
+            if (column == 3) {
+                fwrite(&buffer[i], sizeof(char), 1, out);
+            }
+            if (column == 4) {
+                fwrite(&buffer[i], sizeof(char), 1, out);
+            }
+            if (column == 5) {
+                fwrite(&buffer[i], sizeof(char), 1, out);
+            }
+            if (column == 6) {
+                fwrite(&buffer[i], sizeof(char), 1, out);
+                break;
+            }
+        }
+    }
+    fclose(fp);
+    fclose(out);
     return 0;
 }
